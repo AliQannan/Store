17f88abb26261fda06bb89574ac3e163bb7610fd
EC2 Default User <ec2-user@ip-172-31-34-83.eu-north-1.compute.internal>
2025-08-23 19:10:10 +0000

    clear

--- my-affiliate-store/next.config.ts
+++ my-affiliate-store/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	 images: {
-    domains: ["s.alicdn.com", "offer.alibaba.com"], // ضع هنا أي domain من Alibaba أو أي مصدر خارجي
+    domains: ["s.alicdn.com", "offer.alibaba.com","drive.google.com"], // ضع هنا أي domain من Alibaba أو أي مصدر خارجي
   },
 };
 
