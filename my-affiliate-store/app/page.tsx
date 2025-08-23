c63bd7e77f141fe8c07c39d4172eb23c8a1d94e2
EC2 Default User <ec2-user@ip-172-31-34-83.eu-north-1.compute.internal>
2025-08-23 19:14:10 +0000

    status images

--- my-affiliate-store/app/page.tsx
+++ my-affiliate-store/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <div className="max-w-6xl mx-auto bg-white rounded-xl shadow-md overflow-hidden grid grid-cols-1 md:grid-cols-2 gap-8">
 
       <Image
-      src="https://drive.google.com/file/d/1JRqYc8jitdkTytgeC-oIVdbv3I68WtYn/view?usp=sharing"
+      src="https://drive.google.com/file/d/1JRqYc8jitdkTytgeC-oIVdbv3I68WtYn/view"
       alt="logo"
       width={200}
       height={200}
