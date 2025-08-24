de1548e1cbace22325d56bbd72b3d13c7fdd0daa
AliQannan <126164190+AliQannan@users.noreply.github.com>
2025-08-24 12:11:55 +0300

    Update layout.tsx

--- my-affiliate-store/app/layout.tsx
+++ my-affiliate-store/app/layout.tsx
@@ -42,9 +42,16 @@ export default function RootLayout({
             {/* Logo */}
             <div className="flex-shrink-0">
 
-<div  className="mx-auto" >
-  GS<span className="text-white" >T</span>
-  </div>
+<div className="flex items-center justify-center mx-auto text-5xl font-extrabold tracking-widest">
+  <span className="text-green-600">G</span>
+  <span className="text-black">S</span>
+  <span className="relative inline-block">
+    <span className="text-white bg-black px-2 py-1 rounded-md shadow-md">T</span>
+    <span className="absolute -top-2 -right-3 w-3 h-3 bg-red-600 rounded-full"></span>
+    <span className="absolute -bottom-2 -left-3 w-3 h-3 bg-green-600 rounded-full"></span>
+  </span>
+</div>
+
            </div>
 
             {/* أزرار تسجيل الدخول والتسجيل أو حساب المستخدم */}
