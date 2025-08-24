6c90f6825f8e96707faaf273216aafb5934831d9
AliQannan <126164190+AliQannan@users.noreply.github.com>
2025-08-24 11:33:13 +0300

    Update GSTLogo.js

--- my-affiliate-store/components/GSTLogo.js
+++ my-affiliate-store/components/GSTLogo.js
@@ -1,7 +1,7 @@
 const GSTLogo = ({ size = 100, className = '' }) => {
   return (
     <img 
-      src="/gst-logo.svg" 
+      src="/logo.png" 
       alt="GST Logo with geometric T design" 
       className={className}
       style={{ width: size, height: size }}
