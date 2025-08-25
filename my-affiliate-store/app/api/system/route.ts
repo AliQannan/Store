f8b42e7b1f4afa54b2d721845f2feaf3fd5f86bd
AliQannan <126164190+AliQannan@users.noreply.github.com>
2025-08-25 15:48:41 +0300

    Delete my-affiliate-store/app/api/system directory

--- my-affiliate-store/app/api/system/route.ts
+++
@@ -1,8 +0,0 @@
-import { adminOnly } from './adminOnly';
-import { NextResponse } from 'next/server';
-
-export async function GET(req: Request) {
-  return adminOnly(req, async () => {
-    return NextResponse.json({ message: 'Welcome, admin!' });
-  });
-}
